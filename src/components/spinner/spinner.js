5bd35b56adeedc517f152518de99b3e23a477c02
Alena home <79262812867@ya.ru>
2022-09-05 21:53:24 +0300

    2 2 31m

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './spinner.css';
+
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spin-lnkjitvlgc">
+            <div class="ldio-nxdygvas5mq">
+                <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Spinner;
+
+
+
